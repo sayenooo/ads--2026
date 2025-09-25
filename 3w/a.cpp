ec54a139451fcd392b96d272aeabb91d684be148
sayenooo <zhansaya2007ke@gmail.com>
2025-09-25 18:37:01 +0500

    UPD

--- 3w/a.cpp
+++ 3w/a.cpp
@@ -2,37 +2,33 @@
 using namespace std;
 
 int main() {
-    int x;
-    cin >> x;
-    queue<int> q;
-    while(x--){
-        int y;
-        cin >> y;
-        q.push(y);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-
-    int n,m;
-    cin >> n >> m;
-    map<int,pair<int,int>> mp;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            int y;
-            cin >> y;
-            mp[y] = {i,j};
+    int row, col;
+    cin >> row >> col;
+    map<int, pair<int, int>> mp;
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            int x;
+            cin >> x;
+            mp[x] = {i,j};
         }
     }
-    while(!q.empty()){
-        int val = q.front();
-        if(mp.find(val)!=mp.end()){
-            auto res = mp[val];
-            cout << mp[val].first << " " << mp[val].second << endl;
-            q.pop();
+    
+    for(int i = 0; i<n; i++){
+        if(mp.find(a[i])!=mp.end()){
+            cout << mp[a[i]].first << " " << mp[a[i]].second << endl;
         }else{
             cout << -1 << endl;
-            q.pop();
         }
+        
     }
-
+    
+    
     return 0;
     
 }

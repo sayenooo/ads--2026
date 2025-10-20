893ceaa9a9e3b49f5a58417473f6338e0f6bf7c1
sayenooo <zhansaya2007ke@gmail.com>
2025-10-20 21:20:31 +0500

    upd

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &s, int l, int m, int r){
+bool func(string a, string b){
+    return a<b;
+}
+void merge(vector<string> &s, int l, int r, int m){
     int n1 = m - l + 1;
     int n2 = r - m;
-    int L[n1];
-    int R[n2];
+    vector<string> L(n1);
+    vector<string> R(n2);
     for(int i = 0; i<n1; i++){
-        L[i] = s[l + i];
+        L[i] = s[l+i];
     }
     for(int i = 0; i<n2; i++){
-        R[i] = s[m + 1 + i];
+        R[i] = s[m+1+i];
     }
-    int k = l;
     int uk1 = 0;
     int uk2 = 0;
+    int k = l;
     while(uk1<n1 && uk2<n2){
-        if(L[uk1]<=R[uk2]){
+        if(L[uk1].size() < R[uk2].size()){
             s[k] = L[uk1];
             uk1++;
-        }else{
+        }else if(L[uk1].size() > R[uk2].size()){
             s[k] = R[uk2];
             uk2++;
+        }else{
+            if(func(L[uk1],R[uk2])){
+                s[k] = L[uk1];
+                uk1++;
+            }else{
+                s[k] = R[uk2];
+                uk2++;
+            }
         }
         k++;
     }
@@ -36,38 +47,35 @@ void merge(vector<int> &s, int l, int m, int r){
         k++;
     }
 }
-void mergesort(vector<int> &s , int l, int r){
+void mergesort(vector<string> &s, int l, int r){
     if(l<r){
         int m = (l+r)/2;
         mergesort(s, l, m);
-        mergesort(s, m + 1, r);
-        merge(s, l, m, r);
+        mergesort(s, m+1, r);
+        merge(s, l, r, m);
     }
 }
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n), b(m);
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+    vector<string>a;
 
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < m; i++) cin >> b[i];
-
-    mergesort(a, 0, n - 1);
-    mergesort(b, 0, m - 1);
-
-    int i = 0, j = 0;
-    while (i < n && j < m) {
-        if (a[i] < b[j]) {
-            i++;
-        } else if (a[i] > b[j]) {
-            j++;
-        } else {
-            cout << a[i] << " ";
-            i++;
-            j++;
+    for(int i = 0; i<n; i++){
+        a.clear();
+        string ss;
+        getline(cin, ss);
+        stringstream s(ss);
+        string c;
+        while(s >> c){
+            a.push_back(c);
         }
+        mergesort(a, 0, a.size() - 1);
+        for(int i = 0; i<a.size(); i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
     }
-
     return 0;
 }
